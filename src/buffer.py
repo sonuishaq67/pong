1413dec2d483d1696d57d4d9dedb465fa11d44bb
Ishaq Ahmed Shaik <sonuishaq67@gmail.com>
2026-02-26 05:01:51 -0700

    created model and buffer

---
+++ src/buffer.py
@@ -0,0 +1,48 @@
+import numpy as np
+import torch 
+
+
+class ReplayBuffer():
+    
+    def __init__(self,max_size, input_shape, n_action, device='cpu'):
+        # storing in ram because we're storing a lot of data
+        
+        self.mem_size = max_size
+        self.mem_ctr=0
+        self.state_memory= np.zeros((self.mem_size,*input_shape),dtype=np.uint8)
+        self.next_state_memory= np.zeros((self.mem_size,*input_shape),dtype=np.uint8)
+        self.action_memory = np.zeros(self.mem_size, dtype=np.float32)
+        self.reward_memory = np.zeros(self.mem_size, dtype=np.uint8)
+        self.terminal_memory = np.zeros(self.mem_size,dtype=bool) 
+        self.device = device
+        
+    
+    # sample only when we have enough batches so as to not overfit the model on a small sample
+    
+    def can_sample(self,batch_size) :
+        if self.mem_ctr> (batch_size*5):
+            return True
+        return False
+    
+    def store_transition(self, state, action, reward, next_state, done):
+        # overwrite oldest buffer when mem is full
+        index = self.mem_ctr % self.mem_size
+        self.state_memory[index]= state
+        self.next_state_memory[index]=next_state
+        self.action_memory[index]=torch.tensor(action).detach().cpu()
+        self.reward_memory[index]=reward
+        self.terminal_memory[index]=done
+        self.mem_ctr+=1
+        
+    def sample_buffer(self,batch_size):
+        # max_mem to use if mem is not full
+        max_mem=min(self.mem_ctr,self.mem_size)
+        batch = np.random.choice(max_mem,batch_size)
+        
+        states=torch.tensor(self.state_memory[batch],dtype=torch.float32).to(self.device)
+        actions= torch.tensor(self.action_memory[batch],dtype=torch.float32).to(self.device)
+        rewards=torch.tensor(self.reward_memory[batch],dtype=torch.float32).to(self.device)
+        next_states=torch.tensor(self.next_state_memory[batch],dtype=torch.float32).to(self.device)
+        dones= torch.tensor(self.terminal_memory[batch],dtype=torch.bool).to(self.device)
+        
+        return states, actions, rewards, next_states,dones
---
+++ src/model.py
@@ -0,0 +1,73 @@
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Model(nn.Module):
+    def __init__(self, action_dim, hidden_dim=256, observation_shape=None):
+        super(Model,self).__init__()
+        
+        self.conv1=nn.Conv2d(in_channels=1,out_channels=8,kernel_size=4, stride=2)
+        self.conv2=nn.Conv2d(in_channels=8,out_channels=16,kernel_size=4, stride=2)
+        self.conv3=nn.Conv2d(in_channels=16,out_channels=32,kernel_size=3, stride=2)
+
+        conv_outp_size=self.calc_conv_output(observation_shape)
+        print("conv output size",conv_outp_size)
+
+
+        self.fc1=nn.Linear(conv_outp_size,hidden_dim)
+        self.fc2=nn.Linear(hidden_dim,hidden_dim)
+        self.fc3=nn.Linear(hidden_dim,hidden_dim)
+                
+        self.output=nn.Linear(hidden_dim,action_dim)
+        
+        self.apply(self.init_weights)
+        
+    
+    def forward(self,x):
+        x=x/255
+        x=F.relu(self.conv1(x))
+        x=F.relu(self.conv2(x))
+        x=F.relu(self.conv3(x))
+
+        x=x.view(x.size(0),-1)
+
+        x=F.relu(self.fc1(x))
+        x=F.relu(self.fc2(x))
+        x=F.relu(self.fc3(x))
+    
+        return self.output(x)
+    
+    
+    # helper functions below this
+    
+    def calc_conv_output(self,observation_shape):
+        x= torch.zeros(1,*observation_shape)
+        x=F.relu(self.conv1(x))
+        x=F.relu(self.conv2(x))
+        x=F.relu(self.conv3(x))
+        
+        return x.view(-1).shape[0]
+    
+    
+    def init_weights(self,m):
+        if isinstance(m,nn.Conv2d):
+            nn.init.kaiming_normal_(m.weight,nonlinearity='relu')
+            if m.bias is not None:
+                nn.init.constant_(m.bias,0)
+        elif isinstance(m,nn.Linear):
+            nn.init.xavier_normal_(m.weight)
+            if m.bias is not None:
+                nn.init.constant_(m.bias,0)
+                
+                
+    def save_model(self,filename='models/latest.pt'):
+        torch.save(self.state_dict(),filename)
+
+    def load_mode(self,filename='models/latest.pt'):
+        # torch.save(self.state_dict(),filename)
+        try:
+            self.load_state_dict(torch.load(filename))
+            print("loaded weights from ",filename)
+            
+        except FileNotFoundError:
+            print("No weights found at " , filename)
