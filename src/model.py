1e725cad118de7a2d5d012eac256c13117e3590b
Ishaq Ahmed Shaik <sonuishaq67@gmail.com>
2026-02-26 05:11:06 -0700

    added soft update to model

--- src/model.py
+++ src/model.py
@@ -71,3 +71,8 @@ class Model(nn.Module):
             
         except FileNotFoundError:
             print("No weights found at " , filename)
+            
+            
+def soft_update(target,source,tau=0.005):
+    for target_param, param in zip(target.parameters(),source.parameters()):
+        target_param.data.copy_(target_param.data*(1.0-tau)+param.data*tau)
